f483b1b8c102a0c8d932414162cc5ce855707af4
weilai <1090660448@qq.com>
2026-06-27 20:59:12 +0800

    day01 新增员工(未测试)

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 
@@ -7,9 +8,15 @@ public interface EmployeeService {
 
     /**
      * 员工登录
-     * @param employeeLoginDTO
-     * @return
+     * @param employeeLoginDTO a
+     * @return  a
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
+
+    /**
+     * 新增员工
+     * @param employeeDTO a
+     */
+    void save(EmployeeDTO employeeDTO);
 }
